ce11204448eced6b9e2f111ad29b05d686ae21ed
Nikolay <149093882+NMKRTYCHIAN@users.noreply.github.com>
2024-02-14 18:47:18 +0300

    add login + registr

--- src/components/SignIn/SignInForm.jsx
+++ src/components/SignIn/SignInForm.jsx
@@ -1,19 +1,48 @@
+import { useState } from 'react';
+import axios from 'axios';
 import './SignInForm.scss';
 
 export default function SignInForm() {
+  const [user, setUser] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('https://blog.kata.academy/api/users/login', { user });
+      console.log(`Успех ${res.data}`);
+    } catch (error) {
+      console.log(`не успех ${error}`);
+    }
+  };
+
   return (
     <div className='sign-in-form'>
       <h2>Sign In</h2>
       <form>
         <label htmlFor='email' className='email'>
           Email address
-          <input type='email' id='email' placeholder='Email address' />
+          <input
+            type='email'
+            id='email'
+            placeholder='Email address'
+            value={user.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
         </label>
         <label htmlFor='pass' className='pass'>
           Password
-          <input type='password' id='pass' placeholder='Password' />
+          <input
+            type='password'
+            id='pass'
+            placeholder='Password'
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
         </label>
-        <button>Login</button>
+        <button onClick={handleLogin}>Login</button>
         <span>
           Dont't have an account? <a href='#'>Sign Up.</a>
         </span>
--- src/components/SignUp/SignUpForm.jsx
+++ src/components/SignUp/SignUpForm.jsx
@@ -1,28 +1,70 @@
+import React, { useState } from 'react';
+import axios from 'axios';
 import './SignUpForm.scss';
 
 export default function SignUpForm() {
+  const [user, setUser] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('https://blog.kata.academy/api/users', { user });
+      console.log('Успешно зарегистрирован:', res.data);
+    } catch (error) {
+      console.error('Ошибка при регистрации пользователя:', error);
+    }
+  };
+
   return (
     <div className='sign-up-form'>
       <h2>Sign Up</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor='username' className='username'>
           Username
-          <input type='text' id='username' placeholder='some-username' />
+          <input
+            type='text'
+            id='username'
+            placeholder='some-username'
+            value={user.username}
+            onChange={(e) => setUser({ ...user, username: e.target.value })}
+          />
         </label>
 
         <label htmlFor='email' className='email'>
           Email address
-          <input type='email' id='email' placeholder='Email address' />
+          <input
+            type='email'
+            id='email'
+            placeholder='Email address'
+            value={user.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
         </label>
 
         <label htmlFor='pass' className='pass'>
           Password
-          <input type='password' id='pass' placeholder='Password' />
+          <input
+            type='password'
+            id='pass'
+            placeholder='Password'
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
         </label>
 
         <label htmlFor='repeat'>
           Repeat Password
-          <input type='password' id='repeat' placeholder='Password' />
+          <input
+            type='password'
+            id='repeat'
+            placeholder='Password'
+            value={user.password}
+            onChange={(e) => setUser({ ...user, password: e.target.value })}
+          />
         </label>
 
         <label htmlFor='check' className='check'>
