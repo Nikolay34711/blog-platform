ec5733bebdc23572579ffca20ec5a435113e7aca
Nikolay <149093882+NMKRTYCHIAN@users.noreply.github.com>
2024-02-15 19:25:16 +0300

    refact

--- src/pages/SignIn/SignInForm.jsx
+++ src/pages/SignIn/SignInForm.jsx
@@ -1,54 +1,61 @@
 import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { setAuth } from '../../Redux/slice/sliceAuthentication';
+import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
 import { login } from '../../services/services';
 import './SignInForm.scss';
 
 export default function SignInForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const dispatch = useDispatch();
   const nav = useNavigate();
-  const [user, setUser] = useState({
-    email: '',
-    password: '',
-  });
+  const [submitting, setSubmitting] = useState(false);
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
+  const handleLogin = async (data) => {
     try {
-      const res = await login(user);
+      setSubmitting(true);
+      const res = await login(data);
       dispatch(setAuth(res.data.user));
       nav('/');
     } catch (error) {
       console.error(error);
+    } finally {
+      setSubmitting(false);
     }
   };
 
   return (
     <div className='sign-in-form'>
       <h2>Sign In</h2>
-      <form>
+      <form onSubmit={handleSubmit(handleLogin)}>
         <label htmlFor='email' className='email'>
           Email address
+          {errors.email && <span className='error'>{errors.email.message}</span>}
           <input
             type='email'
             id='email'
             placeholder='Email address'
-            value={user.email}
-            onChange={(e) => setUser({ ...user, email: e.target.value })}
+            {...register('email', { required: 'Email is required' })}
           />
         </label>
         <label htmlFor='pass' className='pass'>
           Password
+          {errors.password && <span className='error'>{errors.password.message}</span>}
           <input
             type='password'
             id='pass'
             placeholder='Password'
-            value={user.password}
-            onChange={(e) => setUser({ ...user, password: e.target.value })}
+            {...register('password', { required: 'Password is required' })}
           />
         </label>
-        <button onClick={handleLogin}>Login</button>
+        <button type='submit' disabled={submitting}>
+          Login
+        </button>
         <span>
           Dont't have an account? <Link to='/sign-up'>Sign Up.</Link>
         </span>
